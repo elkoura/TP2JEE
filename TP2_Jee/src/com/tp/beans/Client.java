ef01ff8cedac7efa2c9fafa7779f0e1027a31c61
elkoura <elkoura.younes@gmail.com>
2014-11-01 21:57:03 -0400

    la base !

--- TP2_Jee/src/com/tp/beans/Client.java
+++ TP2_Jee/src/com/tp/beans/Client.java
@@ -1,5 +1,55 @@
 package com.tp.beans;
 
+
+
 public class Client {
+	
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String telephone;
+	private String email;
+	private String mot_de_passe;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getMot_de_passe() {
+		return mot_de_passe;
+	}
+	public void setMot_de_passe(String mot_de_passe) {
+		this.mot_de_passe = mot_de_passe;
+	}
+	
+	
+	
 
 }
